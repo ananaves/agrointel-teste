2906135d8c7a89226a5d51b46acf636f61cc3a16
Ana Naves <ananaves2003@gmail.com>
2025-03-28 15:35:55 -0300

    terminando menu, inicio e rodape, em css e react

--- src/Components/Inicio/inicio.tsx
+++ src/Components/Inicio/inicio.tsx
@@ -1,7 +1,7 @@
 import estilo from './inicio.module.css'
-// import imgCirculo from '../../assets/Img/';
+import imgc from '../../assets/Img/imagemCirculo.jpg'
 import img1 from '../../assets/Img/imagem.jpg';
-import img2 from '../../assets/Img/imagem.jpg';
+import img2 from '../../assets/Img/imagem2 .jpg'
 import img3 from '../../assets/Img/imagem.webp';
 import icone1 from '../../assets/Img/qualidade.png';
 import icone2 from '../../assets/Img/funcionamento-facil.png';
@@ -12,9 +12,9 @@ import icone4 from '../../assets/Img/planejamento.png';
 function Inicio() {
     return (
         <section className={estilo.inicio}>
-            <div className='sobre-texto'>
+            <div className={estilo.sobretexto}>
                 <h2>Sobre nós</h2>
-                <h3>Inteligência que transforma o seu planejamento de safra</h3>
+                <h4>Inteligência que transforma o seu planejamento de safra</h4>
                 <p> O AgroIntel é um software de planejamento agrícola com inteligência artificial,
                     desenvolvido para produtores de médio porte que buscam mais eficiência e rentabilidade.</p>
 
@@ -23,43 +23,40 @@ function Inicio() {
                     tomar decisões estratégicas com segurança.</p>
 
                 <p>Plante com inteligência. Colha com estratégia.</p>
-
-                {/* <div className='imgSobre'>
-                    <img src={imgCirculo} />
-                </div> */}
-
             </div>
 
-                <div className="destaques">
-                    <div className='qualidade'>
-                        <img src={icone1} />
-                        <p>Recomendações personalizadas</p>
-                    </div>
-                    <div className='funcionamento'>
-                        <img src={icone2} />
-                        <p>Adaptações às necessidades específicas</p>
-                    </div>
-                    <div className='adaptacao'>
-                        <img src={icone3} />
-                        <p> Facilidade de uso</p>
-                    </div>
-                    <div className='planejamento'>
-                        <img src={icone4} />
-                        <p> Planejamento eficiente, produtivo e sustentável</p>
-                    </div>
+            <img className={estilo.imagem} src={imgc} />
+
+            <div className={estilo.destaques}>
+                <div className='qualidade'>
+                    <img src={icone1} />
+                    <p>Recomendações personalizadas</p>
+
+                </div>
+                <div className='funcionamento'>
+                    <img src={icone2} />
+                    <p>Adaptações às necessidades específicas</p>
                 </div>
 
-            
+                <div className={estilo.adaptacao}>
+                    <img src={icone3} />
+                    <p> Facilidade de uso</p>
+                </div>
+                <div className={estilo.planejamento}>
+                    <img src={icone4} />
+                    <p> Planejamento eficiente, produtivo e sustentável</p>
+                </div>
+            </div>
 
-            <section className="imagens">
+            <section className={estilo.imagens}>
                 <img src={img1} />
                 <img src={img2} />
                 <img src={img3} />
             </section>
 
-            <section className="depoimentos">
+            <section className={estilo.depoimentos}>
                 <div className="card">
-                    <h3>QUEM USA RECOMENDA !</h3>
+                    <h5>QUEM USA RECOMENDA !</h5>
                     <p>“Depois do AgroIntel, minha produtividade aumentou e parei de plantar no escuro.”
                         <br />– João S., produtor de milho, MG </p>
                     <p className="estrelas">⭐⭐⭐</p>
