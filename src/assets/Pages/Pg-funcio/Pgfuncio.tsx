0b790defecbf77e3372cda760a2d1013ccdd6d1d
Ana Naves <ananaves2003@gmail.com>
2025-03-28 16:15:26 -0300

    pagina nova

---
+++ src/assets/Pages/Pg-funcio/Pgfuncio.tsx
@@ -0,0 +1,13 @@
+import Menu from "../../../Components/Menu/menu";
+import Rodape from "../../../Components/Rodape/rodape";
+
+function Funcionalidades() {
+    return(
+        <>
+        <Menu/>
+        <Rodape/>
+        </>
+    );
+}
+
+export default Funcionalidades;
